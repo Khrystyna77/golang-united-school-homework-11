0df3e4d3213971993545e2142ee6c52f7905faec
Khrystyna2409 <xrustuna.di@gmail.com>
2022-07-02 20:33:44 +0300

    1

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,8 @@
 package batch
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -14,5 +16,21 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
+	Userchan := make(chan []user, pool)
+	//Userchan := make(chan struct{}, pool)
+	var wg sync.WaitGroup
+	var i int64
+	for i = 0; i < n; i++ {
+		wg.Add(1)
+		go func(nextId int64) {
+			user := getOne(nextId)
+			res = append(res, user)
+			Userchan <- res
+			fmt.Println(res)
+			wg.Done()
+			//close(Userchan)
+		}(i)
+		wg.Wait()
+	}
 	return nil
 }
